a8dcb8777e0e841e0f319ed064992c8b644db8fe
Robbert Jansen <robbert_jansen@me.com>
2022-10-25 21:36:35 +0200

    lul

--- VolumioApp/Pages/HomePage.xaml.cs
+++ VolumioApp/Pages/HomePage.xaml.cs
@@ -1,3 +1,4 @@
+using Bumptech.Glide.Manager;
 using VolumioApp.PageModels;
 
 namespace VolumioApp.Pages;
@@ -9,5 +10,28 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
 		this.BindingContext = homePageModel;
 
+		RotateImage();
+		ScaleImage();
 	}
+
+	private async Task RotateImage()
+	{
+		while (true)
+		{
+			await playButtonImage.RotateTo(360, 5000);
+
+			playButtonImage.Rotation = 0;
+
+        }
+	}
+
+    private async Task ScaleImage()
+    {
+        while (true)
+        {
+			await playButtonImage.ScaleTo(1.05, 2000);
+            await playButtonImage.ScaleTo(1.0, 2000);
+
+        }
+    }
 }
