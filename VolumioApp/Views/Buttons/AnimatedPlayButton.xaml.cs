c103b2e773b7afd77e49ebb74aae38cb9ad44cf4
Robbert Jansen <robbert_jansen@me.com>
2023-01-28 19:20:17 +0100

    Works, but the rotation animation is a bit messed up
    Fix later

--- VolumioApp/Views/Buttons/AnimatedPlayButton.xaml.cs
+++ VolumioApp/Views/Buttons/AnimatedPlayButton.xaml.cs
@@ -11,7 +11,7 @@ public partial class AnimatedPlayButton : ContentView
 
     public static readonly BindableProperty CommandProperty =BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(AnimatedPlayButton));
 
-   
+    public CancellationTokenSource TokenSource { get; set; }
 
     public ICommand Command
     {
@@ -28,8 +28,15 @@ public partial class AnimatedPlayButton : ContentView
 
         if ((bool)newValue)
         {
-            instance.OnInit();
+            instance.StartAnimation();
+            
         }
+        else
+        {
+            instance.ResetAnimation();
+        }
+
+        //instance.OnInit();
 
     }
 
@@ -44,6 +51,8 @@ public partial class AnimatedPlayButton : ContentView
     public AnimatedPlayButton()
 	{
 		InitializeComponent();
+
+        TokenSource = new CancellationTokenSource();
 	}
 
     private void TapGestureRecognizerTapped(object sender, EventArgs e)
@@ -55,9 +64,121 @@ public partial class AnimatedPlayButton : ContentView
 
     }
 
+    private async Task RotateEllipseGrid()
+    {
+        while (IsPlaying)
+        {
+            await EllipseGrid.RotateTo(360, 5000);
+
+            EllipseGrid.Rotation = 0;
+
+            Console.WriteLine("Rotate Grid executed");
+
+        }
+    }
+
+    private async Task ScaleEllipse1()
+    {
+        while (IsPlaying)
+        {
+            Random rnd = new Random();
+            double randomInt = (double)rnd.Next(lowerBound, upperBound);
+            await Ellipse1.ScaleXTo(randomInt / 100, 2000, Easing.CubicInOut);
+            Console.WriteLine("ScaleEllipse1 executed");
+        }
+
+        await Ellipse1.ScaleXTo(1, 1000, Easing.CubicInOut);
+    }
+
+    private async Task ScaleEllipse2()
+    {
+        while (IsPlaying)
+        {
+            Random rnd = new Random();
+            double randomInt = (double)rnd.Next(lowerBound, upperBound);
+            await Ellipse2.ScaleXTo(randomInt / 100, 2000, Easing.CubicInOut);
+            Console.WriteLine("ScaleEllipse1 executed");
+        }
+
+        await Ellipse2.ScaleXTo(1, 1000, Easing.CubicInOut);
+    }
+
+    private async Task ScaleEllipse3()
+    {
+        while (IsPlaying)
+        {
+            Random rnd = new Random();
+            double randomInt = (double)rnd.Next(lowerBound, upperBound);
+            await Ellipse3.ScaleXTo(randomInt / 100, 2000, Easing.CubicInOut);
+            Console.WriteLine("ScaleEllipse1 executed");
+        }
+
+        await Ellipse3.ScaleXTo(1, 1000, Easing.CubicInOut);
+    }
+
+    private async Task ScaleEllipse4()
+    {
+        while (IsPlaying)
+        {
+            Random rnd = new Random();
+            double randomInt = (double)rnd.Next(lowerBound, upperBound);
+            await Ellipse4.ScaleXTo(randomInt / 100, 2000, Easing.CubicInOut);
+            Console.WriteLine("ScaleEllipse1 executed");
+        }
+
+        await Ellipse4.ScaleXTo(1, 1000, Easing.CubicInOut);
+    }
+
+
+
+    private async Task StartAnimation()
+    {
+        RotateEllipseGrid();
+        ScaleEllipse1();
+        ScaleEllipse2();
+        ScaleEllipse3();
+        ScaleEllipse4();
+    }
+
+    private async Task ResetAnimation()
+    {
+        EllipseGrid.CancelAnimations();
+        Ellipse1.CancelAnimations();
+        Ellipse2.CancelAnimations();
+        Ellipse3.CancelAnimations();
+        Ellipse4.CancelAnimations();
+
+        var currentRotation = EllipseGrid.Rotation;
+
+        currentRotation += 72;
+
+        if(currentRotation > 360)
+        {
+            currentRotation -= 360;
+        }
+
+        EllipseGrid.RotateTo(currentRotation, 1000);
+        Ellipse1.ScaleXTo(1, 1000, Easing.CubicInOut);
+        Ellipse2.ScaleXTo(1, 1000, Easing.CubicInOut);
+        Ellipse3.ScaleXTo(1, 1000, Easing.CubicInOut);
+        Ellipse4.ScaleXTo(1, 1000, Easing.CubicInOut);
+
+    }
+
     private async Task OnInit()
     {
         Stopwatch stopwatch = Stopwatch.StartNew();
+
+        if(IsPlaying)
+        {
+            EllipseGrid.CancelAnimations();
+            Ellipse1.CancelAnimations();
+            Ellipse2.CancelAnimations();
+            Ellipse3.CancelAnimations();
+            Ellipse4.CancelAnimations();
+        }
+
+
 
         Task.Factory.StartNew(async () =>
         {
@@ -68,6 +189,7 @@ public partial class AnimatedPlayButton : ContentView
                 EllipseGrid.Rotation = 0;
 
                 Console.WriteLine("Rotate Grid executed");
+
             }
 
         });
