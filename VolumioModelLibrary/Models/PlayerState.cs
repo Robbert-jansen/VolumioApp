56a81be0d345b17aba77276177483bc9f50c2512
Robbert Jansen <robbert_jansen@me.com>
2023-01-29 22:16:32 +0100

    model lib cleanup

--- VolumioModelLibrary/Models/PlayerState.cs
+++ VolumioModelLibrary/Models/PlayerState.cs
@@ -1,85 +1,77 @@
 ﻿using System.ComponentModel;
 
 namespace VolumioModelLibrary.Models;
+
 public class PlayerState : INotifyPropertyChanged
 {
-    public bool IsPlaying
-    {
-        get
-        {
-            if (Status == "play")
-            {
-                return true;
-            }
-            return false;
-        }
-        set
-        {
-            if(Status == "stop")
-            {
-                IsPlaying = false;
-            }
+	private string? _albumArt;
 
-            if(value)
-            {
-                Status = "play";
-            }
-            else
-            {
-                Status = "pause";
-            }
-            IsPlaying = value;
-         }
-    }
+	public bool IsPlaying
+	{
+		get
+		{
+			if (Status == "play") return true;
+			return false;
+		}
+		set
+		{
+			if (Status == "stop") IsPlaying = false;
 
-    public string? Status { get; set; }
-    public int Position { get; set; }
-    public string? Title { get; set; }
-    public string? Artist { get; set; }
-    public string? Album { get; set; }
+			if (value)
+				Status = "play";
+			else
+				Status = "pause";
+			IsPlaying = value;
+		}
+	}
 
-    private string? _albumArt;
-    public string? AlbumArt
-    {
-        get => _albumArt;
-        set
-        {
-            if(value.Contains("http"))
-            {
-                _albumArt =  value;
-            }
-            else
-            {
-                _albumArt = "http://192.168.2.21" + value;
-            }
-            
-        }
-    }
-    public string? Uri { get; set; }
-    public string? TrackType { get; set; }
+	public string? Status { get; set; }
+	public int Position { get; set; }
+	public string? Title { get; set; }
+	public string? Artist { get; set; }
+	public string? Album { get; set; }
 
-    // Current timestamp in miliseconds.
-    public int? Seek { get; set; } 
-    // Duration in seconds.
-    public int Duration { get; set; }
+	public string? AlbumArt
+	{
+		get => _albumArt;
+		set
+		{
+			if (value.Contains("http"))
+				_albumArt = value;
+			else
+				_albumArt = "http://192.168.2.21" + value;
+		}
+	}
 
-    public string? SampleRate { get; set; }
-    public string? BitDepth { get; set; }
-    public bool? Random { get; set; }
-    public bool? Repeat { get; set; }
-    public bool RepeatSingle { get; set; }
-    public bool Consume { get; set; }
-    public int? Volume { get; set; }
-    public object? DbVolume { get; set; }
-    public bool DisableVolumeControl { get; set; }
-    public bool Mute { get; set; }
-    public string? Stream { get; set; }
-    public bool Updatedb { get; set; }
-    public bool Volatile { get; set; }
-    public string? Service { get; set; }
+	public string? Uri { get; set; }
+	public string? TrackType { get; set; }
 
-    public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.UtcNow;
+	// Current timestamp in miliseconds.
+	public int? Seek { get; set; }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+	// Duration in seconds.
+	public int Duration { get; set; }
+
+	public string? SampleRate { get; set; }
+	public string? BitDepth { get; set; }
+	public bool? Random { get; set; }
+	public bool? Repeat { get; set; }
+	public bool RepeatSingle { get; set; }
+	public bool Consume { get; set; }
+	public int? Volume { get; set; }
+	public object? DbVolume { get; set; }
+	public bool DisableVolumeControl { get; set; }
+	public bool Mute { get; set; }
+	public string? Stream { get; set; }
+	public bool Updatedb { get; set; }
+	public bool Volatile { get; set; }
+	public string? Service { get; set; }
+
+	public DateTimeOffset LastUpdated { get; set; } = DateTimeOffset.UtcNow;
+
+	#region INotifyPropertyChanged Members
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	#endregion
 }
-
--- VolumioModelLibrary/Models/Queue.cs
+++ VolumioModelLibrary/Models/Queue.cs
@@ -1,53 +1,55 @@
-﻿using Newtonsoft.Json;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace VolumioModelLibrary.Models;
 
 public class QueueItem : INotifyPropertyChanged
 {
-    
-    public string? Uri { get; set; }
-    public string? Service { get; set; }
-    public string? Name { get; set; }
-    public string? Artist { get; set; }
-    public string? Album { get; set; }
-    public string? Type { get; set; }
-    public int? TrackNumber { get; set; }
-    private string? _albumArt;
+	private string? _albumArt;
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+	public string? Uri { get; set; }
+	public string? Service { get; set; }
+	public string? Name { get; set; }
+	public string? Artist { get; set; }
+	public string? Album { get; set; }
+	public string? Type { get; set; }
+	public int? TrackNumber { get; set; }
 
-    public string? AlbumArt
-    {
-        get => _albumArt;
-        set
-        {
-            if (value.Contains("http"))
-            {
-                _albumArt = value;
-            }
-            else
-            {
-                _albumArt = "http://192.168.2.21" + value;
-                //_albumArt = "http://volumio.local" + value;
-            }
+	public string? AlbumArt
+	{
+		get => _albumArt;
+		set
+		{
+			if (value.Contains("http"))
+				_albumArt = value;
+			else
+				_albumArt = "http://192.168.2.21" + value;
+			//_albumArt = "http://volumio.local" + value;
+		}
+	}
 
-        }
-    }
-    public int? Duration { get; set; }
-    public string? TrackType { get; set; }
-    public string? SampleRate { get; set; }
-    public string? BitDepth { get; set; }
-    public int? Channels { get; set; }
+	public int? Duration { get; set; }
+	public string? TrackType { get; set; }
+	public string? SampleRate { get; set; }
+	public string? BitDepth { get; set; }
+	public int? Channels { get; set; }
 
-    public bool IsPlaying { get; set; }
+	public bool IsPlaying { get; set; }
+
+	#region INotifyPropertyChanged Members
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	#endregion
 }
 
 public class Queue : INotifyPropertyChanged
 {
-    [JsonProperty("Queue")]
-    public List<QueueItem>? QueueItems { get; set; }
+	[JsonProperty("Queue")] public List<QueueItem>? QueueItems { get; set; }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+	#region INotifyPropertyChanged Members
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	#endregion
 }
-
