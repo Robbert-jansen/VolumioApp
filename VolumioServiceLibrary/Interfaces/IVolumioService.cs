0ed55f163d1d7904e952817f2278b2887dd8b3a7
Robbert Jansen <robbert_jansen@me.com>
2023-01-29 22:16:01 +0100

    Service lib cleanup

--- VolumioServiceLibrary/Interfaces/IVolumioService.cs
+++ VolumioServiceLibrary/Interfaces/IVolumioService.cs
@@ -1,92 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VolumioModelLibrary.Models;
+﻿using VolumioModelLibrary.Models;
 
 namespace VolumioServiceLibrary.Interfaces;
 
 public interface IVolumioService
 {
     /// <summary>
-    /// Event that is triggered when a new/updated PlayerState is pushed.
+    ///     Event that is triggered when a new/updated PlayerState is pushed.
     /// </summary>
     public event EventHandler StatePushed;
+
     /// <summary>
-    /// Event that is triggered when a new/updated Queue is pushed.
+    ///     Event that is triggered when a new/updated Queue is pushed.
     /// </summary>
     public event EventHandler QueuePushed;
 
-    #region Rest
+	#region Rest
+
     /// <summary>
-    /// Gets current PlayerState of Volumio Server.
+    ///     Gets current PlayerState of Volumio Server.
     /// </summary>
     /// <returns>Current PlayerState.</returns>
     Task<PlayerState> GetPlayerState();
+
     /// <summary>
-    /// Returns the current Queue of songs.
+    ///     Returns the current Queue of songs.
     /// </summary>
     /// <returns>Current Queue.</returns>
     Task<Queue> GetQueue();
 
     /// <summary>
-    /// Starts playing specified track from the current Queue.
+    ///     Starts playing specified track from the current Queue.
     /// </summary>
     /// <param name="trackPosition">The index of the desired song in the current Queue.</param>
     Task PlayTrackFromQueue(int trackPosition);
-    
+
     /// <summary>
-    /// Toggles between different repeating modes.
+    ///     Toggles between different repeating modes.
     /// </summary>
     /// <param name="playerState">Current PlayerState which will be used to determine the next repeat state.</param>
     Task ToggleRepeat(PlayerState playerState);
-    
+
     /// <summary>
-    /// Toggles between linear and shuffle playback.
+    ///     Toggles between linear and shuffle playback.
     /// </summary>
     /// <param name="playerState">Current PlayerState which will be used to determine the next playback state.</param>
     Task ToggleShuffle(PlayerState playerState);
-    #endregion
 
-    #region Socket.io
+	#endregion
+
+	#region Socket.io
+
     /// <summary>
-    /// Toggles between play and pause states.
+    ///     Toggles between play and pause states.
     /// </summary>
     Task TogglePlayback();
 
     /// <summary>
-    /// Skips to next track in Queue
+    ///     Skips to next track in Queue
     /// </summary>
     Task NextTrack();
 
     /// <summary>
-    /// Skips to previous track in Queue
+    ///     Skips to previous track in Queue
     /// </summary>
     Task PreviousTrack();
 
     /// <summary>
-    /// Mutes volume.
+    ///     Mutes volume.
     /// </summary>
     Task MuteVolume();
 
     /// <summary>
-    /// Unmutes Volume.
+    ///     Unmutes Volume.
     /// </summary>
     /// <returns></returns>
     Task UnmuteVolume();
 
     /// <summary>
-    /// Sets volume to specified percentage.
+    ///     Sets volume to specified percentage.
     /// </summary>
     /// <param name="volume">Integer value between 0 and 100.</param>
     /// <returns></returns>
     Task ChangeVolume(int volume);
 
     /// <summary>
-    /// Moves seek to specified second.
+    ///     Moves seek to specified second.
     /// </summary>
     /// <param name="seconds">Integer value representing whole seconds.</param>
     Task ChangeSeek(int? seconds);
-    #endregion
+
+	#endregion
 }
--- VolumioServiceLibrary/Services/VolumioService.cs
+++ VolumioServiceLibrary/Services/VolumioService.cs
@@ -1,210 +1,189 @@
-﻿using Newtonsoft.Json;
+﻿using System.Diagnostics;
+using Newtonsoft.Json;
 using SocketIOClient;
-using System.Diagnostics;
 using VolumioModelLibrary.Models;
 using VolumioServiceLibrary.Interfaces;
-using Queue = VolumioModelLibrary.Models.Queue;
 
 namespace VolumioServiceLibrary.Services;
 
 public class VolumioService : IVolumioService
 {
-    private static SocketIO? Socket { get; set; }
-    private static HttpClient client = new HttpClient();
+	private static HttpClient client = new();
 
-    // Socket EventHandlers
-    public event EventHandler? StatePushed;
-    public event EventHandler? QueuePushed;
+	public VolumioService()
+	{
+		OnInit();
+	}
 
-    public VolumioService()
-    {    
-        OnInit();
-    }
+	private static SocketIO? Socket { get; set; }
 
-    public async void OnInit()
-    {
-        // Create new socket with server connection.
-        //Socket = new SocketIO("http://volumio.local:3000/");
-        //client.BaseAddress = new Uri("http://volumio.local/api/v1/");
+	#region IVolumioService Members
 
-        //IPs for Android emualtor, Android emulators can't rersolve hostnames.
-        client.BaseAddress = new Uri("http://192.168.2.21/api/v1/");
-        Socket = new SocketIO("http://192.168.2.21:3000/");
+	// Socket EventHandlers
+	public event EventHandler? StatePushed;
+	public event EventHandler? QueuePushed;
 
+	#endregion
 
-        // Creates listeners for events emitted by server.
-        Socket.On("pushState", response =>
-        {
-            Debug.WriteLine("State pushed");
-            PlayerState? playerState = JsonConvert.DeserializeObject<PlayerState>(response.GetValue(0).ToString());
-            if(playerState != null)
-            {
-                StatePushed?.Invoke(playerState, new EventArgs());
-            }              
-        });
+	public async void OnInit()
+	{
+		// Create new socket with server connection.
+		//Socket = new SocketIO("http://volumio.local:3000/");
+		//client.BaseAddress = new Uri("http://volumio.local/api/v1/");
 
-        Socket.On("pushQueue", response =>
-        {
-            Debug.WriteLine("Queue pushed");
-            List<QueueItem> queue = JsonConvert.DeserializeObject<List<QueueItem>>(response.GetValue(0).ToString());
-            if (queue != null)
-            {
-                QueuePushed?.Invoke(queue, new EventArgs());
-            }
-   
-        });
-
-        Socket.OnDisconnected += Socket_OnDisconnected;
-        Socket.OnConnected += Socket_OnConnected;
-
-        // Connects to socket.
-        try
-        {
-            await Socket.ConnectAsync();
-        }
-        catch (Exception e)
-        {
-            Socket = new SocketIO("http://192.168.2.21:3000/");
-            client = new HttpClient();
-            client.BaseAddress = new Uri("http://192.168.2.21/api/v1/");
-            await Socket.ConnectAsync();
-        }
-        
-
-    }
+		//IPs for Android emualtor, Android emulators can't rersolve hostnames.
+		client.BaseAddress = new Uri("http://192.168.2.21/api/v1/");
+		Socket = new SocketIO("http://192.168.2.21:3000/");
 
 
-    #region Rest
+		// Creates listeners for events emitted by server.
+		Socket.On("pushState", response =>
+		{
+			Debug.WriteLine("State pushed");
+			var playerState = JsonConvert.DeserializeObject<PlayerState>(response.GetValue().ToString());
+			if (playerState != null) StatePushed?.Invoke(playerState, new EventArgs());
+		});
 
-    // HTTP Requests.
-    public async Task<Queue> GetQueue()
-    {
-        return (Queue)await GetAsync(typeof(Queue), "getQueue");
-    }
+		Socket.On("pushQueue", response =>
+		{
+			Debug.WriteLine("Queue pushed");
+			var queue = JsonConvert.DeserializeObject<List<QueueItem>>(response.GetValue().ToString());
+			if (queue != null) QueuePushed?.Invoke(queue, new EventArgs());
+		});
 
-    public async Task<PlayerState> GetPlayerState()
-    {
-        return (PlayerState)await GetAsync(typeof(PlayerState), "getState");
-    }
+		Socket.OnDisconnected += Socket_OnDisconnected;
+		Socket.OnConnected += Socket_OnConnected;
 
-    public async Task PlayTrackFromQueue(int trackPosition)
-    {
-        await client.GetAsync("commands/?cmd=play&N=" + trackPosition);
-    }
+		// Connects to socket.
+		try
+		{
+			await Socket.ConnectAsync();
+		}
+		catch (Exception e)
+		{
+			Socket = new SocketIO("http://192.168.2.21:3000/");
+			client = new HttpClient();
+			client.BaseAddress = new Uri("http://192.168.2.21/api/v1/");
+			await Socket.ConnectAsync();
+		}
+	}
 
-    // Is this nececcary? probably not but it's cool.
-    private static async Task<object> GetAsync(Type returnType, string uri)
-    {
-        var response = await client.GetAsync(uri);
 
-        if (response.IsSuccessStatusCode)
-        {
-            var Content = await response.Content.ReadAsStringAsync();
-            object returnObject = JsonConvert.DeserializeObject(Content, returnType);
-            if (returnObject != null)
-            {
-                return returnObject;
-            }
-        }
+	#region Rest
 
-        return Activator.CreateInstance(returnType); 
-    }
+	// HTTP Requests.
+	public async Task<Queue> GetQueue()
+	{
+		return (Queue)await GetAsync(typeof(Queue), "getQueue");
+	}
 
-    #endregion
+	public async Task<PlayerState> GetPlayerState()
+	{
+		return (PlayerState)await GetAsync(typeof(PlayerState), "getState");
+	}
 
-    #region Socket.io
+	public async Task PlayTrackFromQueue(int trackPosition)
+	{
+		await client.GetAsync("commands/?cmd=play&N=" + trackPosition);
+	}
 
-    // Socket connection handlers.
-    private void Socket_OnConnected(object? sender, EventArgs e)
-    {
-        Debug.WriteLine("!!!!!!!!!!!!!!!Socket connected");
-    }
-    private void Socket_OnDisconnected(object? sender, string e)
-    {
-        Debug.WriteLine("!!!!!!!!!!!!!!!Socket Disconnected");
-    }
+	// Is this nececcary? probably not but it's cool.
+	private static async Task<object> GetAsync(Type returnType, string uri)
+	{
+		var response = await client.GetAsync(uri);
 
-    // Socket Emit methods.
-    public async Task TogglePlayback()
-    {
-        await EmitAsync("toggle");
-    }
-    
-    /*public async Task ToggleRepeat(bool repeat, bool repeatSingle)
-    {
-        await EmitAsync("setRepeat", new object[]
-        {
-            new { value = repeat, repeatSingle = repeatSingle }
-        });
-    }*/
+		if (response.IsSuccessStatusCode)
+		{
+			var Content = await response.Content.ReadAsStringAsync();
+			var returnObject = JsonConvert.DeserializeObject(Content, returnType);
+			if (returnObject != null) return returnObject;
+		}
 
-    public async Task ToggleRepeat(PlayerState playerState)
-    {
-        var data = new object();
-        // Checks current state and uses that to determine next state.
-        if (playerState.Repeat == false)
-        {
-            data = new { value = true, repeatSingle = false };
-        }
-        else if (playerState is { Repeat: true, RepeatSingle: false })
-        {
-            data = new { value = true, repeatSingle = true };
-        }
-        else if (playerState.Repeat == true)
-        {
-            data = new { value = false, repeatSingle = false };
-        }
-        
-        await EmitAsync("setRepeat", new[] { data });
-    }
-    
-    public async Task ToggleShuffle(PlayerState playerState)
-    {
-        await EmitAsync("setRandom", new object[] { new { value = !playerState.Random } });
-    }
+		return Activator.CreateInstance(returnType);
+	}
 
-    public async Task NextTrack()
-    {
-        await EmitAsync("next");
-    }
+	#endregion
 
-    public async Task PreviousTrack()
-    {
-        await EmitAsync("prev");
-    }
+	#region Socket.io
 
-    public async Task MuteVolume()
-    {
-        await EmitAsync("mute");
-    }
+	// Socket connection handlers.
+	private void Socket_OnConnected(object? sender, EventArgs e)
+	{
+		Debug.WriteLine("!!!!!!!!!!!!!!!Socket connected");
+	}
 
-    public async Task UnmuteVolume()
-    {
-        await EmitAsync("unmute");
-    }
+	private void Socket_OnDisconnected(object? sender, string e)
+	{
+		Debug.WriteLine("!!!!!!!!!!!!!!!Socket Disconnected");
+	}
 
-    public async Task ChangeVolume(int volume)
-    {
-        await EmitAsync("volume", volume);
-    }
+	// Socket Emit methods.
+	public async Task TogglePlayback()
+	{
+		await EmitAsync("toggle");
+	}
 
-    public async Task ChangeSeek(int? seconds)
-    {
-        if(seconds.HasValue)
-        {
-            await EmitAsync("seek", seconds);
-        }
-    }
+	/*public async Task ToggleRepeat(bool repeat, bool repeatSingle)
+	{
+	    await EmitAsync("setRepeat", new object[]
+	    {
+	        new { value = repeat, repeatSingle = repeatSingle }
+	    });
+	}*/
 
-    // Wraps EmitAsync in method to handle it in one place.
-    private async Task EmitAsync(string eventName, params object[] data)
-    {
-        if(Socket != null && Socket.Connected)
-        {
-            await Socket!.EmitAsync(eventName, data);
-        }
-    }
+	public async Task ToggleRepeat(PlayerState playerState)
+	{
+		var data = new object();
+		// Checks current state and uses that to determine next state.
+		if (playerState.Repeat == false)
+			data = new { value = true, repeatSingle = false };
+		else if (playerState is { Repeat: true, RepeatSingle: false })
+			data = new { value = true, repeatSingle = true };
+		else if (playerState.Repeat == true) data = new { value = false, repeatSingle = false };
 
-    #endregion
+		await EmitAsync("setRepeat", data);
+	}
+
+	public async Task ToggleShuffle(PlayerState playerState)
+	{
+		await EmitAsync("setRandom", new { value = !playerState.Random });
+	}
+
+	public async Task NextTrack()
+	{
+		await EmitAsync("next");
+	}
+
+	public async Task PreviousTrack()
+	{
+		await EmitAsync("prev");
+	}
+
+	public async Task MuteVolume()
+	{
+		await EmitAsync("mute");
+	}
+
+	public async Task UnmuteVolume()
+	{
+		await EmitAsync("unmute");
+	}
+
+	public async Task ChangeVolume(int volume)
+	{
+		await EmitAsync("volume", volume);
+	}
+
+	public async Task ChangeSeek(int? seconds)
+	{
+		if (seconds.HasValue) await EmitAsync("seek", seconds);
+	}
+
+	// Wraps EmitAsync in method to handle it in one place.
+	private async Task EmitAsync(string eventName, params object[] data)
+	{
+		if (Socket != null && Socket.Connected) await Socket!.EmitAsync(eventName, data);
+	}
+
+	#endregion
 }
-
